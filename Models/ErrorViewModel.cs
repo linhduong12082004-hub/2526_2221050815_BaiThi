c82459e08f8c7b271d7097ccfa7c36a1df16c2e5
linhduong12082004-hub <linhduong12082004@gmail.com>
2026-06-19 23:16:36 +0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace _2526_2221050815_BaiThi.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
