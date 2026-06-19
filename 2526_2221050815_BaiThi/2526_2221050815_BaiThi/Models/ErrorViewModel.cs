577711df7bbe607faeee066cb46f2d4b1facd2f9
TranDuongThuyLinh <linhduong12082004@gmail.com>
2026-06-19 23:11:43 +0700

    Create_project

---
+++ 2526_2221050815_BaiThi/2526_2221050815_BaiThi/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace _2526_2221050815_BaiThi.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
